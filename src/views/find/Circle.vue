02ca9c7839b5d5df654e822965c90d2152d8d4de
yangliangfei <yangliangfei@oneband.life>
2017-11-26 18:05:29 +0800

    [FIXBUG]

--- src/views/find/Circle.vue
+++ src/views/find/Circle.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="circle-main">
-    <span>cricle</span>
+    <div class="top">
+      <span class="back">< 发现</span>
+      <span class="tip">朋友圈</span>
+      <span class="publish">发布</span>
+    </div>
+    <div class="cover">
+      <img class="cover-img" src="static/avatar/3.jpg" alt="cover"/>
+      <div class="personal-info">
+        <span class="name">name</span>
+        <img class="avatar" src="static/avatar/4.jpg" alt="avatar"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +22,55 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.circle-main {
+  margin: -8px;
+  margin-right: 0px;
+  .top {
+    display: flex;
+    position: fixed;
+    top: 0px;
+    width: 100%;
+    padding: 3.1% 0px;
+    justify-content: space-between;
+    font-size: 18px;
+    color: white;
+    background: #000;
+    z-index: 999;
+    .back {
+      margin-left: 3%;
+    }
+    .tip {
+      margin-right: 6%;
+    }
+    .publish {
+      margin-right: 5%;
+    }
+  }
+  .cover {
+    .cover-img {
+      width: 102.3%;
+      height: 340px;
+    }
+    .avatar {
+      width: 80px;
+      height: 80px;
+      border: 3px solid white;
+      border-radius: 1px;
+    }
+    .personal-info {
+      position: relative;
+      top: -56px;
+      float: right;
+      .name {
+        position: relative;
+        top: -48px;
+        right: 16px;
+        font-size: 18px;
+        font-weight: bold;
+        color: white;
+        letter-spacing: 1.2px;
+      }
+    }
+  }
+}
 </style>
